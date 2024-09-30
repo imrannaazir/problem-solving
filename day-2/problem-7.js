7565a7fdfb5cf5ed94334010eb228ffdc762ded8
imrannaazir <imrannaaziremon@gmail.com>
2024-10-01 00:33:31 +0600

    Solved : task 7

--- day-2/problem-7.js
+++ day-2/problem-7.js
@@ -1,4 +1,13 @@
-/* 
-Task 6 : Leap Year Checker
-Write a function that determines whether a given year is a leap year.
-*/
+// Task 7: Leap Year Checker
+// Write a function that determines whether a given year is a leap year.
+// if year is divided by 4 but not divided by 100 , or divided by 400 
+
+const checkLeapYear = (year) => {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkLeapYear(2020))
